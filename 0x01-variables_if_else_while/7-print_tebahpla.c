7b3200e29a96fc8d087d61801fa8bd3a4a000e22
Danny Rae <emmanueldan0920@gmail.com>
2022-03-12 17:21:31 +0100

    backwards aplabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * print lowercase alphabet in reverse
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ph;
+
+	for (ph = 'z'; ph >= 'a'; ph--)
+	{
+
+		putchar(ph);
+
+	}
+
+	putchar('\n');
+
+	return (0);
